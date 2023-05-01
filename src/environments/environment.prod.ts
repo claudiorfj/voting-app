fd633ba7d1fea1a9a4f466429688c840005e5035
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-01 15:16:20 +0100

    add environment folder

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    supabaseUrl: 'https://vvzxuwhfsjycgegormyn.supabase.co',
+    supabaseKey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2enh1d2hmc2p5Y2dlZ29ybXluIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI4ODQ5MjgsImV4cCI6MTk5ODQ2MDkyOH0.raQGoMxsDjZ4LD1MRjYMmkxgMMXQ6xxSSxnf1YhVUpw',
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://vvzxuwhfsjycgegormyn.supabase.co',
+    supabaseKey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2enh1d2hmc2p5Y2dlZ29ybXluIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI4ODQ5MjgsImV4cCI6MTk5ODQ2MDkyOH0.raQGoMxsDjZ4LD1MRjYMmkxgMMXQ6xxSSxnf1YhVUpw',
+  };
+  
+  /*
+   * For easier debugging in development mode, you can import the following file
+   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+   *
+   * This import should be commented out in production mode because it will have a negative impact
+   * on performance if an error is thrown.
+   */
+  // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
